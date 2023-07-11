2d8c6a2fbc72a556dd314df4f3c9cbecbee3c0d5
Chicken2k <dangconglan2k@gmail.com>
2023-07-11 10:37:01 +0700

    update comment

---
+++ src/modules/comments/dto/filter-comments.dto.ts
@@ -0,0 +1,109 @@
+import { Comment } from 'entities/comments';
+import {
+  NumberFieldOptional,
+  StringFieldOptional,
+  ObjectFieldOptional,
+} from 'src/decorators/field.decorator';
+import { IsstatusEnum as PartyIsstatusEnum } from 'entities/parties';
+
+export class FilterCommentRequest {
+  @NumberFieldOptional({ int: true })
+  party_id?: number;
+  @NumberFieldOptional({ int: true })
+  user_id?: number;
+  @StringFieldOptional({ maxLength: 65535, minLength: 0 })
+  comment?: string;
+  @NumberFieldOptional({ int: true, minimum: -2147483647, maximum: 2147483646 })
+  id_cmtrep?: number;
+}
+export class FilterCommentRequestDTO {
+  @NumberFieldOptional({ int: true })
+  pagination_page?: number;
+  @NumberFieldOptional({ int: true })
+  pagination_limit?: number;
+  @ObjectFieldOptional(FilterCommentRequest)
+  comments?: FilterCommentRequest;
+}
+export class PartyFilterCommentResponse {
+  id: number;
+  created_at: Date;
+  updated_at: Date;
+  nameparty: string;
+  partystarttime: Date;
+  partylocation: string;
+  numberofpeople: number;
+  isstatus: `${PartyIsstatusEnum}`;
+  admin_id: number;
+  describe: string;
+  requiredage: number;
+  img: string;
+}
+export class UserFilterCommentResponse {
+  id: number;
+  created_at: Date;
+  updated_at: Date;
+  isactive: boolean;
+  username: string;
+  password: string;
+  dateofbirth: Date;
+  email: string;
+}
+export class FilterCommentResponse {
+  id: number;
+  created_at: Date;
+  updated_at: Date;
+  party: PartyFilterCommentResponse;
+  party_id: number;
+  user: UserFilterCommentResponse;
+  user_id: number;
+  comment: string;
+  id_cmtrep: number;
+}
+export class FilterMessageResponse {}
+
+export class FilterCommentResponseDTO {
+  comments: FilterCommentResponse[];
+  total_pages?: number;
+  message?: Object;
+  total_count: number;
+
+  constructor(comments: Comment[], total_count: number, total_pages?: number, message?: Object) {
+    this.comments = comments.map((comment) => ({
+      ...comment,
+      id: comment?.id,
+      created_at: comment?.created_at,
+      updated_at: comment?.updated_at,
+      party: {
+        id: comment?.party?.id,
+        created_at: comment?.party?.created_at,
+        updated_at: comment?.party?.updated_at,
+        nameparty: comment?.party?.nameparty,
+        partystarttime: comment?.party?.partystarttime,
+        partylocation: comment?.party?.partylocation,
+        numberofpeople: comment?.party?.numberofpeople,
+        isstatus: comment?.party?.isstatus,
+        admin_id: comment?.party?.admin_id,
+        describe: comment?.party?.describe,
+        requiredage: comment?.party?.requiredage,
+        img: comment?.party?.img,
+      },
+      party_id: comment?.party_id,
+      user: {
+        id: comment?.user?.id,
+        created_at: comment?.user?.created_at,
+        updated_at: comment?.user?.updated_at,
+        isactive: comment?.user?.isactive,
+        username: comment?.user?.username,
+        password: comment?.user?.password,
+        dateofbirth: comment?.user?.dateofbirth,
+        email: comment?.user?.email,
+      },
+      user_id: comment?.user_id,
+      comment: comment?.comment,
+      id_cmtrep: comment?.id_cmtrep,
+    }));
+    this.total_pages = total_pages;
+    this.message = message;
+    this.total_count = total_count;
+  }
+}
